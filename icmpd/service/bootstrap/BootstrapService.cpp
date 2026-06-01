310f8e60c20b27af20c3bc187991c21281fb263f
tty0929 <jhkim000929@gmail.com>
2026-06-01 17:24:59 +0900

    2026-06-01

--- icmpd/service/bootstrap/BootstrapService.cpp
+++ icmpd/service/bootstrap/BootstrapService.cpp
@@ -11,17 +11,36 @@ BootstrapService::BootstrapService()
 
 void BootstrapService::start()
 {
+    m_state = State::Init;
+    m_startedAt = std::chrono::steady_clock::now();
     
+    LOG_INFO("BootstrapService start...");
 }
 
 std::unique_ptr<nf::event::Event> BootstrapService::schedule(std::chrono::steady_clock::time_point now)
 {
-    return nullptr;
+    std::unique_ptr<nf::event::Event> event = nullptr;
+
+    if (m_state == State::Init)
+    {
+        LOG_DEBUG("Create SendClientHello Event");
+        event = std::make_unique<BootstrapEvent>(BootstrapEventType::SendClientHello);
+
+        /* temp implement */
+        m_state = State::WaitServerHello;
+    }
+
+    return event;
 }
 
 bool BootstrapService::isReady()
 {
     return false;
+}
+
+void BootstrapService::handleEvent(const IcmpdEvent& event)
+{
+    LOG_INFO("handle event");
 }
 
 } // namespace nf::icmpd
--- icmpd/service/bootstrap/BootstrapService.h
+++ icmpd/service/bootstrap/BootstrapService.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "event/IcmpdEvent.h"
+#include "service/bootstrap/BootstrapEvent.h"
 
 #include <memory>
 #include <chrono>
@@ -17,6 +17,22 @@ public:
     void start();
     std::unique_ptr<nf::event::Event> schedule(std::chrono::steady_clock::time_point now);
     bool isReady();
+
+    void handleEvent(const IcmpdEvent& event);
+
+private:
+    enum class State
+    {
+        Init, 
+        WaitServerHello,
+        Ready,
+        Failed
+    };
+
+    State m_state {State::Init};
+
+    std::chrono::steady_clock::time_point m_startedAt {};
+    std::chrono::steady_clock::time_point m_lastClientHelloSentAt {};
 };
 
 } // namespace nf::icmpd
